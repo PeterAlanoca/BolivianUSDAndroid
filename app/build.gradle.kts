35f2f9daa5ab02f3fc07caa1112c16c9d18bea97
Peter Alanoca <peter.alanoca@gmail.com>
2025-07-08 19:38:52 -0400

    add version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         applicationId = "com.bolivianusd.app"
         minSdk = 23
         targetSdk = 34
-        versionCode = System.getenv("BUILD_NUMBER")?.toIntOrNull() ?: 6
+        versionCode = System.getenv("BUILD_NUMBER")?.toIntOrNull() ?: 7
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
